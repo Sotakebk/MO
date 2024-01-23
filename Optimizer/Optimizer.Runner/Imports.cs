9e27be7e66fda587938adc8e6ee3a94d056a2ecc
dawidox2424 <dawidox24@poczta.fm>
2024-01-23 23:25:21 +0100

    addded new Import and Solution class

---
+++ Optimizer/Optimizer.Runner/Imports.cs
@@ -0,0 +1,85 @@
+﻿using Optimizer.Logic;
+
+namespace Optimizer.Runner;
+using System.IO;
+using System.Linq;
+using OfficeOpenXml;
+
+
+public static class Imports
+{
+    public static (Input, Solution) ImportData(string defenseFilePath, string chairpersonsFilePath)
+    {
+        var input = new Input();
+        var solution = new Solution();
+
+        // Import data from obrony.xlsx file
+        using (var excelPackage = new ExcelPackage(new FileInfo(defenseFilePath)))
+        {
+            var worksheet = excelPackage.Workbook.Worksheets.First();
+            var rowCount = worksheet.Dimension.Rows;
+
+            for (var row = 2; row <= rowCount; row++)
+            {
+                // var lastName = worksheet.Cells[row, 1].Value?.ToString();
+                // var firstName = worksheet.Cells[row, 2].Value?.ToString();
+                var supervisor = worksheet.Cells[row, 7].Value?.ToString();
+                var reviewer = worksheet.Cells[row, 8].Value?.ToString();
+
+                // Convert names to IDs using Solution class
+                byte supervisorId = solution.PersonId(supervisor);
+                byte reviewerId = solution.PersonId(reviewer);
+
+                // Add to input
+                input.DefensesToAssign = input.DefensesToAssign.Append(new InputCombination
+                {
+                    ReviewerId = reviewerId,
+                    PromoterId = supervisorId,
+                    TotalCount = 1
+                }).ToArray();
+            }
+        }
+
+        // Import data from chairpersons file
+        using (var excelPackage = new ExcelPackage(new FileInfo(chairpersonsFilePath)))
+        {
+            var worksheet = excelPackage.Workbook.Worksheets.First();
+            var rowCount = worksheet.Dimension.Rows;
+
+            for (var row = 2; row <= rowCount; row++)
+            {
+                var chairpersonName = worksheet.Cells[row, 1].Value?.ToString();
+                // Convert chairperson name to ID using Solution class
+                byte chairpersonId = solution.PersonId(chairpersonName);
+
+                // Add to available chairpersons
+                input.AvailableChairPersonIds = input.AvailableChairPersonIds.Append(chairpersonId).ToArray();
+            }
+        }
+
+        // Add days and classrooms to input (you may need to modify this based on your actual structure)
+        input.Days = new[]
+        {
+            new InputDay
+            {
+                Id = 1,
+                Classrooms = new[]
+                {
+                    new InputClassroom(0, slots: 17, 8, 9),
+                    new InputClassroom(1, slots: 16, 8, 8),
+                },
+            },
+            new InputDay
+            {
+                Id = 2,
+                Classrooms = new[]
+                {
+                    new InputClassroom(0, slots: 16, 8, 8),
+                    new InputClassroom(1, slots: 16, 8, 8),
+                },
+            },
+        };
+
+        return (input, solution);
+    }
+}
---
+++ Optimizer/Optimizer.Runner/Solution.cs
@@ -0,0 +1,101 @@
+﻿using Optimizer.Logic;
+using Optimizer.Runner;
+public class Solution
+{
+    private Dictionary<string, byte> _persons = new Dictionary<string, byte>();
+    private Dictionary<byte, DefenseInfo> _defenseInfos = new Dictionary<byte, DefenseInfo>();
+    private byte _nextId = 0;
+
+    public Solution()
+    {
+    }
+
+    public byte PersonId(string unique)
+    {
+        if (_persons.TryGetValue(unique, out var existId))
+            return existId;
+        else
+        {
+            var id = _nextId++;
+            _persons.Add(unique, id);
+            return id;
+        }
+    }
+
+    public void AddDefenseInfo(byte personId, int dayId, int classroomId, byte assignmentA, byte assignmentB, byte chairPersonId)
+    {
+        if (!_defenseInfos.ContainsKey(personId))
+        {
+            _defenseInfos[personId] = new DefenseInfo();
+        }
+
+        _defenseInfos[personId].Add(new DefenseSlot
+        {
+            DayId = dayId,
+            ClassroomId = classroomId,
+            AssignmentA = assignmentA,
+            AssignmentB = assignmentB,
+            ChairPersonId = chairPersonId
+        });
+    }
+
+    public Input GetOptimizerInput()
+    {
+        var input = new Input();
+
+        // Available ChairPersons
+        input.AvailableChairPersonIds = _persons.Values.ToArray();
+
+        // DefensesToAssign
+        input.DefensesToAssign = _defenseInfos.SelectMany(pair => pair.Value
+            .GroupBy(defenseSlot => (reviewer: pair.Key, supervisor: pair.Key))
+            .Select(group => new InputCombination
+            {
+                ReviewerId = group.Key.reviewer,
+                PromoterId = group.Key.supervisor,
+                TotalCount = group.Count()
+            })).ToArray();
+
+        // Days
+        input.Days = _defenseInfos.Values
+            .SelectMany(defenseInfo => defenseInfo
+                .GroupBy(defenseSlot => defenseSlot.DayId)
+                .Select(group => new InputDay
+                {
+                    Id = group.Key,
+                    Classrooms = group
+                        .GroupBy(defenseSlot => defenseSlot.ClassroomId)
+                        .Select(classroomGroup => new InputClassroom(classroomGroup.Key, slots: 0)
+                        {
+                            InputSlots = classroomGroup
+                                .Select(defenseSlot => new InputSlot
+                                {
+                                    Preferences = new[]
+                                    {
+                                        new InputSlotPreference
+                                        {
+                                            PersonId = defenseSlot.ReviewerId,
+                                            PreferenceType = PreferenceType.Preferred
+                                        }
+                                    }
+                                }).ToArray()
+                        }).ToArray()
+                })).ToArray();
+
+        return input;
+    }
+
+
+    private class DefenseInfo : List<DefenseSlot>
+    {
+    }
+
+    private class DefenseSlot
+    {
+        public int DayId { get; set; }
+        public int ClassroomId { get; set; }
+        public byte AssignmentA { get; set; }
+        public byte AssignmentB { get; set; }
+        public byte ChairPersonId { get; set; }
+    }
+}
