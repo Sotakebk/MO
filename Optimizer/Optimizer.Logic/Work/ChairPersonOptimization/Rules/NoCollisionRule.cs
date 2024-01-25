a378b3d2bd8c1c80bc5ef260f454fb8be21f8c32
Jan Bartula <jan.bartula@allmedica.pl>
2024-01-25 20:59:05 +0100

    No collision rule fix

--- Optimizer/Optimizer.Logic/Work/ChairPersonOptimization/Rules/NoCollisionRule.cs
+++ Optimizer/Optimizer.Logic/Work/ChairPersonOptimization/Rules/NoCollisionRule.cs
@@ -4,21 +4,19 @@ internal static class NoCollisionRule
 {
     internal static bool PassesRule(OptimizerAction action, OptimizerState state, TransformedInput tInput)
     {
-        
         if (tInput
                 .Days[action.Position.Day]
                 .Classrooms[action.Position.Classroom]
                 .Blocks[action.Position.Block]
-                .ChairPeoplePreferences[action.ChairPersonId]
-                .NotAllowedCount > 0)
+                .ChairPeoplePreferences.TryGetValue(action.ChairPersonId, out var preference)
+            && preference.NotAllowedCount > 0)
             return false;
-        
+
         if (!state.ChairPersonWorkingAssignmentsAsChairPersonCounts.TryGetValue(action.ChairPersonId, out _))
             return true; // did not get assigned yet (fast lookup)
 
         var sDay = state.Days[action.Position.Day];
         var tDay = tInput.Days[action.Position.Day];
-
 
         var tBlock = tDay.Classrooms[action.Position.Classroom].Blocks[action.Position.Block];
         var (x1, x2) = (Start: tBlock.First, End: tBlock.Last);
